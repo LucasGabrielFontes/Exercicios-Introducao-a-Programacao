1517092f38d8869d21b8f73d76b32150d2d8a458
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-02-25 18:53:08 -0300

    Update phideEuler.c

--- Exercícios Extras/phideEuler.c
+++ Exercícios Extras/phideEuler.c
@@ -1,77 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define P 15
+int calculaMdc (int a, int b) {
 
-void calculaprimos (int vetor[], int t) {
-    int i = 0, a = 2;
-    while (i < t) {
-        int teste = 0;
-        for (int aux = 1; aux <= a; aux++) {
-            if ((a % aux) == 0) {
-                teste += 1;
-            }
-        }
-        if (teste == 2) {
-            vetor[i] = a;
-            i += 1;
-        }
-        a += 1;
+    while (b != 0) {
+
+        int temp = b;
+        b = a%b;
+        a = temp;
+
     }
+
+    return a;
 }
 
-int calculamdc (int a, int b, int primos[], int p) {
-    int mdc = 1;
-    for (int i = 0; i < p; i++) {
-        if (((a % primos[i]) == 0) && ((b % primos[i]) == 0)) {
-            mdc = mdc * primos[i];
-            a = a/primos[i];
-            b = b/primos[i];
-        }
-    }
-    return mdc;
-}
+int calculaPhi (int numero) {
 
-int calculaphi (int numero, int primos[]) {
-    int x = 1, phi = 0;
-    for (; x <= numero; x++) {
-        if (calculamdc(numero, x, primos, P) == 1) {
+    int phi = 0;
+
+    for (int x = 1; x <= numero; x++) {
+
+        if (calculaMdc(numero, x) == 1) {
             phi += 1;
+
         }
     }
+
     return phi;
 }
 
 int main () {
-    int vetorprimos[P], a, phi, teste;
-    calculaprimos(vetorprimos, P);
-    printf("Digite o numero: ");
-    scanf("%d", &a);
-    phi = calculaphi(a, vetorprimos);
-    printf("O Phi de Euler de %d e: %d\n\n", a, phi);
-    printf("Digite o numero da acao que voce deseja realizar: \n\n");
-    printf("1- Testar outro numero;\n");
-    printf("2- Sair\n");
-    printf("Opcao: ");
-    scanf("%d", &teste);
-    while ((teste != 1) && (teste != 2)) {
-        printf("Opcao invalida! Digite corretamente: ");
-        scanf("%d", &teste);
-    }
-    while (teste == 1) {
-        printf("\n-----------------------------\n\n");
-        printf("Digite o numero: ");
+
+    int opcao; // Armazena a opcão digitada pelo usuário para continuar, ou não, a execução do código
+
+    do {
+
+        int a; // Número digitado pelo usuário
+        int phi; // Resultado de Phi de Euler do número
+
+        printf("Digite um numero inteiro positivo para saber o PHI de Euler dele: ");
         scanf("%d", &a);
-        phi = calculaphi(a, vetorprimos);
-        printf("O Phi de Euler de %d e: %d\n\n", a, phi);
+        while (a <= 0) {
+            printf("Número inválido! O número precisa ser um inteiro positivo: ");
+            scanf("%d", &a);
+        }
+
+        phi = calculaPhi(a);
+
+        printf("\nPhi de Euler de %d: %d\n\n", a, phi);
+        printf("=======================================\n\n");
+
         printf("Digite o numero da acao que voce deseja realizar: \n\n");
         printf("1- Testar outro numero;\n");
-        printf("2- Sair\n");
+        printf("2- Sair;\n\n");
         printf("Opcao: ");
-        scanf("%d", &teste);
-        while ((teste != 1) && (teste != 2)) {
+        scanf("%d", &opcao);
+
+        while ((opcao != 1) && (opcao != 2)) {
             printf("Opcao invalida! Digite corretamente: ");
-            scanf("%d", &teste);
+            scanf("%d", &opcao);
         }
-    }
+
+        system("cls");
+
+    } while (opcao == 1);
+
     return 0;
 }
