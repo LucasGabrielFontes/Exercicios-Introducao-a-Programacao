673add5a105eff579c8696ea9077ddc9c5451864
Lucas Gabriel Fontes da Silva <lucasgabrielfontesdasilva@gmail.com>
2024-05-26 09:00:50 -0300

    Melhorias de código

--- Exercícios Extras/phideEuler.c
+++ Exercícios Extras/phideEuler.c
@@ -1,53 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculaMdc (int a, int b) {
-
-    while (b != 0) {
-
-        int temp = b;
-        b = a%b;
-        a = temp;
-
-    }
-
-    return a;
-}
-
-int calculaPhi (int numero) {
-
-    int phi = 0;
-
-    for (int x = 1; x <= numero; x++) {
-
-        if (calculaMdc(numero, x) == 1) {
-            phi += 1;
-
-        }
-    }
-
-    return phi;
-}
+int calculaPhi (int numero);
+int calculaMdc (int a, int b);
 
 int main () {
 
-    int opcao; // Armazena a opcão digitada pelo usuário para continuar, ou não, a execução do código
+    int opcao; // Armazena a opcao digitada pelo usuario para continuar, ou nao, a execucao do codigo
 
     do {
 
-        int a; // Número digitado pelo usuário
-        int phi; // Resultado de Phi de Euler do número
+        int num; // Numero digitado pelo usuario
+        int phi; // Resultado do Phi de Euler do numero
 
-        printf("Digite um numero inteiro positivo para saber o PHI de Euler dele: ");
-        scanf("%d", &a);
-        while (a <= 0) {
-            printf("Número inválido! O número precisa ser um inteiro positivo: ");
-            scanf("%d", &a);
+        printf("Deseja obter o PHI de Euler de qual numero? ");
+        scanf("%d", &num);
+        while (num <= 0) {
+            printf("\nNumero invalido! E necessario ser um inteiro positivo: ");
+            scanf("%d", &num);
         }
 
-        phi = calculaPhi(a);
+        phi = calculaPhi(num);
 
-        printf("\nPhi de Euler de %d: %d\n\n", a, phi);
+        printf("\nResultado: %d\n\n", phi);
         printf("=======================================\n\n");
 
         printf("Digite o numero da acao que voce deseja realizar: \n\n");
@@ -66,4 +41,30 @@ int main () {
     } while (opcao == 1);
 
     return 0;
+}
+
+int calculaPhi (int numero) {
+
+    int phi = 0;
+
+    for (int x = 1; x <= numero; x++) {
+        if (calculaMdc(numero, x) == 1) {
+
+            phi++;
+
+        }
+    }
+
+    return phi;
+
+}
+
+int calculaMdc (int a, int b) { // Utiliza o Algoritmo de Euclides e recursividade para calcular o MDC entre dois numeros
+
+    if (b == 0) {
+        return a;
+    } else {
+        return calculaMdc(b, a%b);
+    }
+
 }
